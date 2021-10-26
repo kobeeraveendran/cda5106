e785ca82ac86ff75ed33338c423b8f6b93c20908
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-10-25 20:50:34 -0400

    add test case 5 (passes)

--- hw/MachineProblem1_Fall2021/cachesim/run_tests.sh
+++ hw/MachineProblem1_Fall2021/cachesim/run_tests.sh
@@ -47,5 +47,14 @@ else
     echo "Validation 4: PASS"
 fi
 
+./sim_cache 16 1024 1 8192 4 0 0 go_trace.txt > output.txt
+DIFF=$(diff -iw output.txt ../validation_runs/validation5.txt)
+if [[ -n $DIFF ]]
+then
+    echo "Validation 5: FAIL"
+else
+    echo "Validation 5: PASS"
+fi
+
 # remove generated .o files, sim_cache binaries, and output.txt file
 rm -f *.o sim_cache
