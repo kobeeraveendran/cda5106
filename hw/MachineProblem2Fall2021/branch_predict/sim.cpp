10ade08bc38b208bb5d12f9d89ee399491c822a4
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-11-12 00:19:40 -0500

    add generic gshare class (supports bimodal)

--- hw/MachineProblem2Fall2021/branch_predict/sim.cpp
+++ hw/MachineProblem2Fall2021/branch_predict/sim.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -22,7 +23,7 @@ class Smith
             max_count = count * 2 - 1;
         }
 
-        int predict(int outcome)
+        void predict(int outcome)
         {
             int prediction;
 
@@ -46,29 +47,138 @@ class Smith
 
             // update counter accordingly
 
-            // if we actually took the branch, increment
+            // if we actually took the branch, increment until saturation
             if (outcome == 1)
             {
                 count = min(count + 1, max_count);
             }
             else
             {
+                // if we didn't take the branch, decrement until saturation
                 count = max(count - 1, 0);
             }
-
-            return prediction;
         }
 
         void print_results()
         {
             float mispred_rate = (float) mispreds / (float) total_preds * 100;
+            mispred_rate = roundf(mispred_rate * 100) / 100;
 
             cout << "OUTPUT" << endl;
             cout << "number of predictions:\t\t" << total_preds << endl;
             cout << "number of mispredictions:\t" << mispreds << endl;
-            cout.precision(4);
-            cout << "misprediction rate:\t\t\t" << mispred_rate << "%" << endl;
+            cout << "misprediction rate:\t\t\t"  << fixed << setprecision(2) << mispred_rate << "%" << endl;
             cout << "FINAL COUNTER CONTENT:\t\t" << count << endl;
+        }
+};
+
+class Gshare
+{
+    private:
+        int mispreds = 0, total_preds = 0;
+        // these are the default counter params as specified in the doc
+        int counter = 4, max_count = 7;
+        int pc_bits, bhr_bits;
+        vector<int> table;
+        int bhr = 0;
+
+    public:
+
+        Gshare(){}
+
+        Gshare(int pc, int bhr)
+        {
+            pc_bits = pc;
+            bhr_bits = bhr;
+            int table_size = pow(2, pc_bits);
+
+            for (int i = 0; i < table_size; i++)
+            {
+                table.push_back(counter);
+            }
+        }
+
+        void predict(int address, int outcome)
+        {
+            total_preds++;
+
+            // discard lower 2 bits of the PC address
+            address >>= 2;
+            
+            // compute the PC's contribution using its lower n bits
+            int pc_index = address & ((int) pow(2, pc_bits) - 1);
+
+            // compute the Gshare table index
+            // note that if n=0, this always evaluates to the pc_index above
+            int index = (bhr ^ pc_index);
+
+            // make prediction based on table[index]'s counter
+            int prediction;
+
+            if (table[index] >= 4)
+            {
+                // predict taken
+                prediction = 1;
+            }
+
+            else
+            {
+                //predict not taken
+                prediction = 0;
+            }
+
+            if (prediction != outcome)
+            {
+                mispreds++;
+            }
+
+            // evaluate our prediction and update counters as needed
+            if (outcome == 1)
+            {
+                table[index] = min(table[index] + 1, max_count);
+            }
+
+            else
+            {
+                table[index] = max(table[index] - 1, 0);
+            }
+
+            // update the branch history register if n > 0
+            if (bhr_bits > 0)
+            {
+                bhr >>= 1;
+                
+                if (outcome)
+                {
+                    bhr |= (int) pow(2, bhr_bits - 1);
+                }
+            }
+        }
+
+        void print_results()
+        {
+            float mispred_rate = (float) mispreds / (float) total_preds * 100;
+            mispred_rate = roundf(mispred_rate * 100) / 100;
+
+            cout << "OUTPUT" << endl;
+            cout << "number of predictions:\t\t" << total_preds << endl;
+            cout << "number of mispredictions:\t" << mispreds << endl;
+            cout << "misprediction rate:\t\t\t" << fixed << setprecision(2) << mispred_rate << "%" << endl;
+
+            if (bhr_bits > 0)
+            {
+                cout << "FINAL GSHARE CONTENTS" << endl;
+            }
+
+            else
+            {
+                cout << "FINAL BIMODAL CONTENTS" << endl;
+            }
+
+            for (int i = 0; i < table.size(); i++)
+            {
+                cout << i << "\t\t" << table[i] << endl;
+            }
         }
 };
 
@@ -77,6 +187,7 @@ int main(int argc, char *argv[])
     string pred_type = argv[1];
     string trace_path;
     Smith predictor;
+    Gshare gshare_predictor;
 
     if (pred_type == "smith")
     {
@@ -89,13 +200,15 @@ int main(int argc, char *argv[])
     {
         int pc_bits = stoi(argv[2]);
         trace_path = argv[3];
+        gshare_predictor = Gshare(pc_bits, 0);
     }
 
     else if (pred_type == "gshare")
     {
         int pc_bits = stoi(argv[2]);
-        int gbh_bits = stoi(argv[3]);
+        int bhr_bits = stoi(argv[3]);
         trace_path = argv[4];
+        gshare_predictor = Gshare(pc_bits, bhr_bits);
     }
 
     cout << "COMMAND" << endl;
@@ -125,9 +238,22 @@ int main(int argc, char *argv[])
             {
                 predictor.predict(outcome_flag);
             }
+
+            else
+            {
+                // use the generic Gshare predictor
+                gshare_predictor.predict(stoi(address, nullptr, 16), outcome_flag);
+            }
         }
 
-        predictor.print_results();
+        if (pred_type == "smith")
+        {
+            predictor.print_results();
+        }
+        else
+        {
+            gshare_predictor.print_results();
+        }
     }
 
 
