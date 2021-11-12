f42b066e0f4af2e302140ae4c0573b76db9a2a6c
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2021-11-12 00:29:28 -0500

    add test cases for the remaining validation files

--- hw/MachineProblem2Fall2021/branch_predict/run_tests.sh
+++ hw/MachineProblem2Fall2021/branch_predict/run_tests.sh
@@ -5,6 +5,13 @@ make
 smith_bits=(3 1 4)
 smith_traces=("gcc_trace.txt" "jpeg_trace.txt" "perl_trace.txt")
 
+gshare_pc_bits=(9 14 11)
+gshare_bhr_bits=(3 8 5)
+gshare_traces=("gcc_trace.txt" "gcc_trace.txt" "jpeg_trace.txt")
+
+bimodal_bits=(6 12 4)
+bimodal_traces=("gcc_trace.txt" "gcc_trace.txt" "jpeg_trace.txt")
+
 # smith validation runs
 for i in {0..2};
 do
@@ -18,6 +25,33 @@ do
         echo "Smith Validation ${FILE_IND}: PASS"
     fi
 done
+
+for i in {0..2};
+do
+    FILE_IND=$((i+1))
+    ./sim gshare ${gshare_pc_bits[$i]} ${gshare_bhr_bits[$i]} ${gshare_traces[$i]} > outputs/gshare_${FILE_IND}.txt
+    DIFF=$(diff -iw outputs/gshare_${FILE_IND}.txt ../validation_runs/val_gshare_${FILE_IND}.txt)
+    if [[ -n $DIFF ]]
+    then
+        echo "Gshare Validation ${FILE_IND}: FAIL"
+    else
+        echo "Gshare Validation ${FILE_IND}: PASS"
+    fi
+done
+
+for i in {0..2};
+do
+    FILE_IND=$((i+1))
+    ./sim bimodal ${bimodal_bits[$i]} ${bimodal_traces[$i]} > outputs/bimodal_${FILE_IND}.txt
+    DIFF=$(diff -iw outputs/bimodal_${FILE_IND}.txt ../validation_runs/val_bimodal_${FILE_IND}.txt)
+    if [[ -n $DIFF ]]
+    then
+        echo "Bimodal Validation ${FILE_IND}: FAIL"
+    else
+        echo "Bimodal Validation ${FILE_IND}: PASS"
+    fi
+done
+    
 
 # remove generated .o files, sim binaries
 rm -f *.o sim
